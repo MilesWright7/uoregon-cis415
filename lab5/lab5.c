a1062b9062dd77821b8e35c8eaf5a2e74723fdfb
MilesWright7 <mwright7@bitbucket.org>
2019-10-30 12:08:45 -0700

    lab5 done

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -16,31 +16,64 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
 /*---------------------------------------------------------------------------*/
+void signal_handler(int signal);
+void signal_init();
+struct sigaction sigact;
+void send_signal(pid_t *pool, int size, int sig);
 
 /*-----------------------------Program Main----------------------------------*/
 int	 main()
 {
 	//variable definitions
-	pid_t  pid, w;
+	pid_t  pid[5];
+	pid_t w;
 	int wstatus, i;
 	i=0;
+	for(i = 0; i < 5; i++){
+		pid[i] = fork();
 	
-	pid = fork();
-	if (pid == 0) {
-		printf("	Child Process: %i - Starting infinite loop...\n", getpid());
-		while(1) {
-			i++;
-			if(i%10000) {
-			printf("	Child Process: %i - Running infinite loop...\n", getpid());
-			i=0
+		if (pid[i] == 0) {
+			printf("	Child Process: %i - Starting infinite loop...\n", getpid());
+			signal_init();
+			while(1) {
+				i++;
+				if(i%10000000) {
+					printf("	Child Process: %i - Running infinite loop...\n", getpid());
+					i=0;
+				}
 			}
+		} else {
+			//else this is an existing proc i.e. the parent
 		}
-	} else {
-		//else this is an existing proc i.e. the parent
-		printf("Parent Process: %i, Waiting for child to finish...\n", getpid());
-		w = waitpid(pid, &wstatus, WUNTRACED | WCONTINUED);
-		printf("All child processes joined. Exiting.\n");
+	}	
+	printf("Parent Process: %i, Waiting for child to finish...\n", getpid());
+	send_signal(pid, 5, SIGUSR1);
+	send_signal(pid, 5, SIGUSR1);
+	send_signal(pid, 5, SIGINT);
+	for(i = 0; i < 5; i++){
+		w = waitpid(pid[i], &wstatus, WUNTRACED | WCONTINUED);
+	}
+	printf("All child processes joined. Exiting.\n");
+	
+
+}
+void signal_init(){
+	sigact.sa_handler = signal_handler;
+	sigaction(SIGUSR1, &sigact, NULL);
+}
+void signal_handler(int signal){
+	sigset_t *set;
+	printf("Child Process: %d - Received signal: %d\n", getpid(), signal);
+	sigwait(set, &signal);
+}
+void send_signal(pid_t *pool, int size, int sig){
+	int j = 0;
+	for(j = 0; j < size; j++){
+		kill(pool[j], sig);
 	}
 }
+
 /*-----------------------------Program End-----------------------------------*/
